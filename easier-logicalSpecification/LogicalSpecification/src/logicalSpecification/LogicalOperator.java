be4b8b55abecef2dc80ba62e768a1ebae2c25b09
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2020-06-16 14:32:08 +0200

    fix merge with logicalSpecification branch

---
+++ easier-logicalSpecification/LogicalSpecification/src/logicalSpecification/LogicalOperator.java
@@ -0,0 +1,17 @@
+/**
+ */
+package logicalSpecification;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Logical Operator</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see logicalSpecification.LogicalSpecificationPackage#getLogicalOperator()
+ * @model abstract="true"
+ * @generated
+ */
+public interface LogicalOperator extends Operator {
+} // LogicalOperator
---
+++ easier-logicalSpecification/LogicalSpecification/src/logicalSpecification/impl/LogicalOperatorImpl.java
@@ -0,0 +1,37 @@
+/**
+ */
+package logicalSpecification.impl;
+
+import logicalSpecification.LogicalOperator;
+import logicalSpecification.LogicalSpecificationPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Logical Operator</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public abstract class LogicalOperatorImpl extends OperatorImpl implements LogicalOperator {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected LogicalOperatorImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return LogicalSpecificationPackage.Literals.LOGICAL_OPERATOR;
+	}
+
+} //LogicalOperatorImpl
