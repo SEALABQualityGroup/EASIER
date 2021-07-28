adc4d9c52dbe80450d45797baf0fb47c542b4ed2
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2021-07-28 15:38:00 +0200

    clean up no longer used code

--- easier-core/src/main/java/it/univaq/disim/sealab/metaheuristic/evolutionary/experiment/RExperimentBuilder.java
+++ easier-core/src/main/java/it/univaq/disim/sealab/metaheuristic/evolutionary/experiment/RExperimentBuilder.java
@@ -15,55 +15,6 @@ public class RExperimentBuilder<S extends RSolution, Result> extends ExperimentB
 		super(experimentName);
 	}
 	
-	/*
-	 * public RExperimentBuilder<S, Result>
-	 * setAlgorithmList(List<ExperimentAlgorithm<S, Result>> algorithmList) { //
-	 * this.algorithmList = new ArrayList<>(algorithmList);
-	 * super.setAlgorithmList(algorithmList); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result>
-	 * setProblemList(List<ExperimentProblem<S>> problemList) { // this.problemList
-	 * = problemList; super.setProblemList(problemList); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setExperimentBaseDirectory(String
-	 * experimentBaseDirectory) { // this.experimentBaseDirectory =
-	 * experimentBaseDirectory + "/" + experimentName;
-	 * super.setExperimentBaseDirectory(experimentBaseDirectory); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setReferenceFrontDirectory(String
-	 * referenceFrontDirectory) { // this.referenceFrontDirectory =
-	 * referenceFrontDirectory;
-	 * super.setReferenceFrontDirectory(referenceFrontDirectory); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setReferenceFrontFileNames(List<String>
-	 * referenceFrontFileNames) { // this.referenceFrontFileNames =
-	 * referenceFrontFileNames;
-	 * super.setReferenceFrontFileNames(referenceFrontFileNames); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result>
-	 * setIndicatorList(List<GenericIndicator<S>> indicatorList) { //
-	 * this.indicatorList = indicatorList; super.setIndicatorList(indicatorList);
-	 * return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setOutputParetoFrontFileName(String
-	 * outputParetoFrontFileName) { // this.outputParetoFrontFileName =
-	 * outputParetoFrontFileName;
-	 * super.setOutputParetoFrontFileName(outputParetoFrontFileName); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setOutputParetoSetFileName(String
-	 * outputParetoSetFileName) { // this.outputParetoSetFileName =
-	 * outputParetoSetFileName;
-	 * super.setOutputParetoSetFileName(outputParetoSetFileName); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setIndependentRuns(int independentRuns)
-	 * { // this.independentRuns = independentRuns;
-	 * super.setIndependentRuns(independentRuns); return this; }
-	 * 
-	 * public RExperimentBuilder<S, Result> setNumberOfCores(int numberOfCores) { //
-	 * this.numberOfCores = numberOfCores; super.setNumberOfCores(numberOfCores);
-	 * return this; }
-	 */
-
 	public RExperiment<S, Result> build() {
 		return new RExperiment<S, Result>(this);
 	}
