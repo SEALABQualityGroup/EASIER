8017dc6d840b335996ceec878a868141c1f11f20
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-02-10 11:16:20 +0100

    update test and remove the no longer used method

--- easier-epsilon/src/test/java/it/univaq/disim/sealab/epsilon/EpsilonStandaloneTest.java
+++ easier-epsilon/src/test/java/it/univaq/disim/sealab/epsilon/EpsilonStandaloneTest.java
@@ -1,36 +1,54 @@
-//package it.univaq.disim.sealab.epsilon;
-//
-//import java.net.URISyntaxException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//
-//import org.eclipse.epsilon.eol.IEolModule;
-//import org.eclipse.epsilon.eol.models.IModel;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import it.univaq.disim.sealab.epsilon.egl.EGLStandalone;
-//
-//public class EpsilonStandaloneTest {
-//
-//	private Path mmaemiliaFilePath;
-//	private final int MAX_THREADS = 64;
-//	
-//	@Before
-//	public void init() throws URISyntaxException {
-//		mmaemiliaFilePath = Paths.get(getClass().getResource("/mm/mmAEmilia.ecore").toURI());
-//	}
-//	
-//	
-//	@Test
-//	public void createEmfModelTestMultiThread() throws Exception {
-//		Path destFilePath; 
-//		for(int i = 0; i < MAX_THREADS ; i++) {
-//			destFilePath = Files.createTempFile("thread-" + String.valueOf(i), "");
-//			
-//			new EGLStandalone().setMetamodelPath(mmaemiliaFilePath).setModel(destFilePath).execute(destFilePath);
-//		}
-//	}
-//	
-//}
+package it.univaq.disim.sealab.epsilon;
+
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.eclipse.epsilon.evl.parse.EvlParser.evlModule_return;
+import org.junit.Before;
+import org.junit.Test;
+
+import it.univaq.disim.sealab.epsilon.eol.EasierUmlModel;
+import it.univaq.disim.sealab.epsilon.evl.EVLStandalone;
+
+public class EpsilonStandaloneTest {
+
+	String refactoringLibraryModule, uml2lqnModule, GQAM_NAMESPACE;
+	String modelPath;
+	EVLStandalone evlModule;
+	
+	@Before
+	public void setUp() {
+		
+		refactoringLibraryModule = Paths.get(FileSystems.getDefault().getPath("").toAbsolutePath().toString(), "..",
+				"easier-refactoringLibrary", "evl", "AP-UML-MARTE.evl").toString();
+		uml2lqnModule = Paths.get(FileSystems.getDefault().getPath("").toAbsolutePath().toString(), "..",
+				"easier-uml2lqn", "org.univaq.uml2lqn").toString();
+
+		GQAM_NAMESPACE = "MARTE::MARTE_AnalysisModel::GQAM::";
+		
+		modelPath = getClass().getResource("/simplified-cocome/cocome.uml").getFile();
+		
+		evlModule = new EVLStandalone();
+		EasierUmlModel uml = null;
+		try {
+			uml = EpsilonStandalone.createUmlModel(modelPath);
+		} catch (EolModelLoadingException | URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		evlModule.setModel(uml);
+		evlModule.setSource(Paths.get(refactoringLibraryModule));
+		evlModule.setParameter(0.95f, "float", "prob_to_be_pa");
+		
+	}
+	
+	
+	@Test
+	public void extractFuzzyValuesTest() {
+		evlModule.extractFuzzyValues();
+	}
+	
+}
