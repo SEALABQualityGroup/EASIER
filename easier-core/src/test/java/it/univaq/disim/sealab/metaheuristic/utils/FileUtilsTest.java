d6ecceec3a186070bda5d845b50330c8cac9564a
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-04-14 09:16:30 +0200

    update tests

--- easier-core/src/test/java/it/univaq/disim/sealab/metaheuristic/utils/FileUtilsTest.java
+++ easier-core/src/test/java/it/univaq/disim/sealab/metaheuristic/utils/FileUtilsTest.java
@@ -1,55 +1,95 @@
 package it.univaq.disim.sealab.metaheuristic.utils;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.nio.file.Files;
-import java.util.stream.Collectors;
+import java.nio.file.Path;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FileUtilsTest {
-	
-	@Before
-	public void setup() {}
 
-	@After
-	public void tearDown() throws IOException {	}
-	
+	@BeforeClass
+	public static void setup() throws IOException {
+		Files.createDirectories(Configurator.eINSTANCE.getOutputFolder());
+	}
+
+	@AfterClass
+	public static void tearDown() throws IOException {
+		Files.deleteIfExists(Configurator.eINSTANCE.getOutputFolder().resolve("algo_perf_stats.csv"));
+		Files.deleteIfExists(Configurator.eINSTANCE.getOutputFolder().resolve("solution_dump.csv"));
+		Files.deleteIfExists(Configurator.eINSTANCE.getOutputFolder().resolve("search_budget_stats.csv"));
+		
+		Files.deleteIfExists(Configurator.eINSTANCE.getOutputFolder());
+		
+	}
+
 	@Test
 	public void solutionDumpTestIfDumpFileContains2Lines() throws IOException {
 		String line = "NSGA-II,problemTest,1,0.15,7,9,0.99";
-		new FileUtils().solutionDumpToCSV(line);		
-		LineNumberReader lnr = new LineNumberReader(new FileReader(Configurator.eINSTANCE.getOutputFolder().resolve("solution_dump.csv").toString()));
+		new FileUtils().solutionDumpToCSV(line);
+		
+		//Check the correct header
+		String header = "algorithm,problem_tag,solID,perfQ,#changes,pas,reliability";
+		assertEquals(header, extractHeaderFromFile(Configurator.eINSTANCE.getOutputFolder().resolve("solution_dump.csv")));
+		assertEquals(7, header.split(",").length);
+		
+		LineNumberReader lnr = new LineNumberReader(
+				new FileReader(Configurator.eINSTANCE.getOutputFolder().resolve("solution_dump.csv").toString()));
 		lnr.lines().count();
 		assertTrue(lnr.getLineNumber() == 2);
-		Files.delete(Configurator.eINSTANCE.getOutputFolder().resolve("solution_dump.csv"));
-		
+
 	}
-	
+
 	@Test
 	public void searchBudgetDumpTestIfDumpFileContains2Lines() throws IOException {
 		String line = "NSGAII,simplified-cocome__BRF_clone_1.23__moc_1.23__mcnn_1.23__moncnn_1.23__MaxEval_12__ProbPAs_0.00,byPrematureConvergence,1,12";
-		new FileUtils().searchBudgetDumpToCSV(line);		
-		LineNumberReader lnr = new LineNumberReader(new FileReader(Configurator.eINSTANCE.getOutputFolder().resolve("search_budget_stats.csv").toString()));
+		new FileUtils().searchBudgetDumpToCSV(line);
+		
+		//Check the correct header
+		String header = "algorithm,problem_tag,search_budget,iteration,max_iteration";
+		assertEquals(header, extractHeaderFromFile(Configurator.eINSTANCE.getOutputFolder().resolve("search_budget_stats.csv")));
+		assertEquals(5, header.split(",").length);
+		
+		LineNumberReader lnr = new LineNumberReader(
+				new FileReader(Configurator.eINSTANCE.getOutputFolder().resolve("search_budget_stats.csv").toString()));
 		lnr.lines().count();
 		assertTrue(lnr.getLineNumber() == 2);
-		Files.delete(Configurator.eINSTANCE.getOutputFolder().resolve("search_budget_stats.csv"));
+	}
+
+	private String extractHeaderFromFile(Path file) throws IOException {
+		String line = "";
+		try (BufferedReader br = new BufferedReader(new FileReader(file.toFile()))) {
+			line = br.readLine(); // Read the first line, and it should be the header
+		}
+		assertNotEquals("", line);
+		return line;
 	}
 
 	@Test
 	public void algoPerfDumpDumpTestIfDumpFileContains2Lines() throws IOException {
 		String line = "PESA2,simplified-cocome__BRF_clone_1.23__moc_1.64__mcnn_1.45__moncnn_1.80__MaxEval_102__ProbPAs_0.95,86805,622854144,449879328,1363148800,969885368";
-		new FileUtils().algoPerfStatsDumpToCSV(line);		
-		LineNumberReader lnr = new LineNumberReader(new FileReader(Configurator.eINSTANCE.getOutputFolder().resolve("algo_perf_stats.csv").toString()));
+		new FileUtils().algoPerfStatsDumpToCSV(line);
+
+		//Check the correct header
+		String EXPECTED_HEADER = "algorithm,problem_tag,execution_time(ms),total_memory_before(B),free_memory_before(B),total_memory_after(B),free_memory_after(B)";
+		String header = extractHeaderFromFile(Configurator.eINSTANCE.getOutputFolder().resolve("algo_perf_stats.csv"));
+		assertEquals(EXPECTED_HEADER, header);
+		
+		assertEquals(7, header.split(",").length);
+		
+		LineNumberReader lnr = new LineNumberReader(
+				new FileReader(Configurator.eINSTANCE.getOutputFolder().resolve("algo_perf_stats.csv").toString()));
 		lnr.lines().count();
 		assertTrue(lnr.getLineNumber() == 2);
-		Files.delete(Configurator.eINSTANCE.getOutputFolder().resolve("algo_perf_stats.csv"));
 	}
-	
+
 }
