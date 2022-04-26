c8a038e919de78a738f4cae3a0912c039b770822
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-04-26 19:36:00 +0200

    change refactoring actions management
    move from the logicalSpecification to hashMap.
    remove the dependency with the LogicalSpecification metamodel

--- easier-uml/src/main/java/it/univaq/disim/sealab/metaheuristic/actions/uml/UMLCloneNode.java
+++ easier-uml/src/main/java/it/univaq/disim/sealab/metaheuristic/actions/uml/UMLCloneNode.java
@@ -3,96 +3,71 @@
  */
 package it.univaq.disim.sealab.metaheuristic.actions.uml;
 
-import java.io.ObjectInputFilter.Config;
 import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
-import org.eclipse.uml2.uml.Node;
-import org.eclipse.uml2.uml.UMLFactory;
-import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
 import it.univaq.disim.sealab.epsilon.EpsilonStandalone;
 import it.univaq.disim.sealab.epsilon.eol.EOLStandalone;
 import it.univaq.disim.sealab.epsilon.eol.EasierUmlModel;
 import it.univaq.disim.sealab.metaheuristic.actions.RefactoringAction;
-import it.univaq.disim.sealab.metaheuristic.evolutionary.RSolution;
 import it.univaq.disim.sealab.metaheuristic.evolutionary.UMLRSolution;
-import it.univaq.disim.sealab.metaheuristic.managers.Manager;
-import it.univaq.disim.sealab.metaheuristic.managers.ocl.uml.UMLOclStringManager;
 import it.univaq.disim.sealab.metaheuristic.utils.Configurator;
-import it.univaq.disim.sealab.metaheuristic.utils.EasierLogger;
-import logicalSpecification.AndOperator;
-import logicalSpecification.ExistsOperator;
-import logicalSpecification.FOLSpecification;
-import logicalSpecification.Parameter;
-import logicalSpecification.PostCondition;
-import logicalSpecification.PreCondition;
-import logicalSpecification.SingleValuedParameter;
-import logicalSpecification.actions.UML.UMLPackage;
-import logicalSpecification.actions.UML.impl.UMLAddNodeActionImpl;
+import org.eclipse.uml2.uml.Component;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Node;
 
-public class UMLCloneNode extends UMLAddNodeActionImpl implements RefactoringAction {
+public class UMLCloneNode implements RefactoringAction {
 
 	private final static Path eolModulePath;
 
 	private final static double BRF = 1.23;
 
-	private final String sourceModelPath;
+	private String sourceModelPath;
+	private double numOfChanges;
+	private final String name;
 
-	private Node targetObject, umlClonedNode;
-
-	private SingleValuedParameter umlNodeToCloneSVP, umlClonedNodeSVP;
+	Map<String, Set<String>> targetElements = new HashMap<>();
+	Map<String, Set<String>> createdElements = new HashMap<>();
 
 	static {
 		eolModulePath = Paths.get(FileSystems.getDefault().getPath("").toAbsolutePath().toString(), "..",
 				"easier-refactoringLibrary", "easier-ref-operations", "clone_node.eol");
 	}
 
-	public static Path getEolModulePath() {
-		return eolModulePath;
+	public UMLCloneNode(String modelPath, Map<String, Set<String>> availableElements) {
+		this();
+
+		sourceModelPath = modelPath;
+		Set<String> availableNode = availableElements.get(UMLRSolution.SupportedType.NODE.toString());
+		Set<String> targetElement = new HashSet<>();
+		targetElement.add(availableNode.stream().skip(new Random().nextInt(availableNode.size())).findFirst().orElse(null));
+		targetElements.put(UMLRSolution.SupportedType.NODE.toString(), targetElement);
+		String clonedNode = targetElement.iterator().next() + "_" + generateHash();
+		createdElements.put(UMLRSolution.SupportedType.NODE.toString(),
+				Set.of(clonedNode));
 	}
 
-	public UMLCloneNode(UMLRSolution sol) {
-
-		sourceModelPath = sol.getModelPath().toString();
-
-		targetObject = getRandomNode(sol);
-
-		umlClonedNode = createNewNode(sol);
-
-		cost = numOfChanges = calculateCost();
-		//numOfChanges = cost;
-
+	public UMLCloneNode() {
 		name = "UMLCloneNode";
-
-		setParameters();
-		createPreCondition();
-		createPostCondition();
-
-	}
-	
-	public double getNumOfChanges() {
-		return numOfChanges;
-//		return calculateCost();
 	}
 
-	private double calculateCost() {
+	public double computeArchitecturalChanges(Collection<?> modelContents) {
+
+		Node targetObject =
+				(Node) modelContents.stream().filter(Node.class::isInstance).
+						map(NamedElement.class::cast).
+						filter(ne -> ne.getName().equals(targetElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next())).findFirst().get();
 
 		int cpSize = targetObject.getCommunicationPaths().size();
 
-		int artSize = (int) targetObject.getDeployments().stream().flatMap(d -> d.getDeployedArtifacts().stream()).count();
-		
-		double brf = Configurator.eINSTANCE.getBRF("clone");
+		int artSize = (int) targetObject.getDeployments().stream().mapToLong(d -> d.getDeployedArtifacts().size()).sum();
 
-		return (cpSize + artSize) * brf;
+		return (cpSize + artSize);
 
 	}
 
@@ -105,54 +80,8 @@ public class UMLCloneNode extends UMLAddNodeActionImpl implements RefactoringAct
 				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
 	}
 
-	private Node createNewNode(UMLRSolution sol) {
-
-		org.eclipse.uml2.uml.Package deploymentView = null;
-		// Retrieves the deployment view package
-		for (Object pkg : EcoreUtil.getObjectsByType(sol.getDirtyIModel().allContents(), UMLPackage.Literals.PACKAGE)) {
-			if (pkg instanceof org.eclipse.uml2.uml.Package
-					&& "deployment_view".equals(((org.eclipse.uml2.uml.Package) pkg).getName())) {
-				deploymentView = (org.eclipse.uml2.uml.Package) pkg;
-				break;
-			}
-		}
-
-		Node node = UMLFactory.eINSTANCE.createNode();
-		node.setName(targetObject.getName() + "_" + generateHash());
-
-		deploymentView.getPackagedElements().add(node);
-
-		return node;
-
-	}
-
-	// Returns a random Node to be cloned
-	private Node getRandomNode(UMLRSolution sol) {
-
-		org.eclipse.uml2.uml.Package deploymentView = null;
-		org.eclipse.uml2.uml.Model rootPackage = null;
-
-		for (Object pkg : EcoreUtil.getObjectsByType(sol.getDirtyIModel().allContents(), UMLPackage.Literals.PACKAGE)) {
-			if (pkg instanceof org.eclipse.uml2.uml.Model) {
-				rootPackage = (org.eclipse.uml2.uml.Model) pkg;
-				break;
-			}
-		}
-
-		for (Object pkg : EcoreUtil.getObjectsByType(rootPackage.getOwnedElements(), UMLPackage.Literals.PACKAGE)) {
-			if (pkg instanceof org.eclipse.uml2.uml.Package
-					&& "deployment_view".equals(((org.eclipse.uml2.uml.Package) pkg).getName())) {
-				deploymentView = (org.eclipse.uml2.uml.Package) pkg;
-				break;
-			}
-		}
-		List<Object> nodes = new ArrayList<>(
-				EcoreUtil.getObjectsByType(deploymentView.getOwnedElements(), UMLPackage.Literals.NODE));
-		return (Node) nodes.get(JMetalRandom.getInstance().nextInt(0, nodes.size() - 1));
-	}
-
 	@Override
-	public void execute() {
+	public void execute() throws RuntimeException {
 		EOLStandalone executor = new EOLStandalone();
 
 		EasierUmlModel contextModel;
@@ -163,17 +92,17 @@ public class UMLCloneNode extends UMLAddNodeActionImpl implements RefactoringAct
 			executor.setModel(contextModel);
 			executor.setSource(eolModulePath);
 
-			// fills variable within the eol module
-			executor.setParameter(targetObject.getName(), "String", "targetNodeName");
-			executor.setParameter(umlClonedNode.getName(), "String", "clonedNodeName");
+			executor.setParameter(targetElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next(), "String", "targetNodeName");
+			executor.setParameter(createdElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next(), "String", "clonedNodeName");
 
 			executor.execute();
 		} catch (EolRuntimeException e) {
-			System.err.println("Error in execution the eolmodule " + eolModulePath);
-			System.err.println(String.format("No Node called '%s' ", targetObject.getName()));
-//			e.printStackTrace();
+			String message = String.format("Error in execution the eolmodule %s%n", eolModulePath);
+			message += e.getMessage();
+			throw new RuntimeException(message);
 		}catch (URISyntaxException e) {
-			EasierLogger.logger_.severe(String.format("ERROR while reading the model", sourceModelPath));
+			String message = String.format("ERROR while reading the model \t %s %n", sourceModelPath);
+			throw new RuntimeException(message);
 		}
 
 		executor.clearMemory();
@@ -181,121 +110,49 @@ public class UMLCloneNode extends UMLAddNodeActionImpl implements RefactoringAct
 	}
 
 	@Override
-	public void createPreCondition() {
-		PreCondition preCondition = Manager.createPreCondition();
-
-		FOLSpecification specification = Manager.createFOLSpectification("CloneNodePreCondition");
-
-		ExistsOperator existsTargetInNodes = Manager.createExistsInCollectionOperator(umlNodeToCloneSVP,
-				getAllNodesMVP());
-
-		AndOperator andRoot = Manager.createAndOperator();
-		andRoot.getArguments().add(existsTargetInNodes);
-
-		specification.setRootOperator(andRoot);
-		preCondition.setConditionFormula(specification);
-		setPre(preCondition);
-	}
-
-	@Override
-	public void createPostCondition() {
-		PostCondition postCondition = Manager.createPostCondition();
-		FOLSpecification specification = Manager.createFOLSpectification("CloneNodePostCondition");
-
-		ExistsOperator existsTargetInNodes = Manager.createExistsInCollectionOperator(umlNodeToCloneSVP,
-				getAllNodesMVP());
-
-		ExistsOperator existsClonedInNodes = Manager.createExistsInCollectionOperator(umlClonedNodeSVP,
-				getAllNodesMVP());
-
-		AndOperator andRoot = Manager.createAndOperator();
-		andRoot.getArguments().add(existsTargetInNodes);
-		andRoot.getArguments().add(existsClonedInNodes);
-
-		specification.setRootOperator(andRoot);
-		postCondition.setConditionFormula(specification);
-		setPost(postCondition);
-
-	}
-
-	@Override
-	public void setParameters() {
-		List<Parameter> params = new ArrayList<>();
-
-		// checks wheter the random node exists within the model
-		umlNodeToCloneSVP = Manager.createSingleValueParameter(UMLOclStringManager.getNodeQuery(targetObject));
-		umlClonedNodeSVP = Manager.createSingleValueParameter(UMLOclStringManager.getNodeQuery(umlClonedNode));
-
-		params.add(umlNodeToCloneSVP);
-		params.add(umlClonedNodeSVP);
-
-		setAllNodesMVP(Manager.createMultipleValuedParameter(UMLOclStringManager.getAllNodesQuery()));
-
-		params.add(getAllNodesMVP());
-		getParameters().addAll(params);
-	}
-
-	@Override
-	public RefactoringAction clone(RSolution solution) {
-
-		UMLCloneNode cloned = new UMLCloneNode((UMLRSolution) solution);
-
-		cloned.setNumOfChanges(this.getNumOfChanges());
-		cloned.setCost(this.getCost());
-		cloned.setName(this.getName());
-
+	public RefactoringAction clone() {
 		try {
-			((UMLRSolution) solution).getDirtyIModel().deleteElement(cloned.getClonedNode());
-		} catch (EolRuntimeException e) {
-			e.printStackTrace();
+			return (RefactoringAction) super.clone();
+		} catch (CloneNotSupportedException e) {
+			throw new RuntimeException(e);
 		}
-
-		cloned.setTargetObject(this.targetObject);
-		cloned.setUmlClonedNode(this.umlClonedNode);
-
-		cloned.parameters = this.getParameters();
-		cloned.pre = this.getPre();
-		cloned.post = this.getPost();
-
-		return cloned;
-
 	}
 
-	public Node getTargetObject() {
-		return this.targetObject;
+	@Override
+	public String getTargetType() {
+		return UMLRSolution.SupportedType.NODE.toString();
 	}
 
-	public Node getClonedNode() {
-		return this.umlClonedNode;
+	@Override
+	public Map<String, Set<String>> getTargetElements() {
+		return targetElements;
 	}
 
-	public void setUmlClonedNode(Node n) {
-		this.umlClonedNode = n;
-	}
-
-	public void setTargetObject(Node n) {
-		this.targetObject = n;
+	@Override
+	public Map<String, Set<String>> getCreatedElements() {
+		return createdElements;
 	}
 
 	@Override
 	public String toString() {
-		return "Cloning --> " + targetObject.getName() + " with -->  " + umlClonedNode.getName();
+		return "Cloning --> " + targetElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next() + " " +
+				"with -->  " + createdElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next();
 	}
 	
 	public String toCSV() {
-		return String.format("UMLCloneNode,%s,%s,", targetObject.getName(), umlClonedNode.getName());
+		return String.format("UMLCloneNode,%s,%s,",
+				targetElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next(),
+				createdElements.get(UMLRSolution.SupportedType.NODE.toString()).iterator().next());
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((sourceModelPath == null) ? 0 : sourceModelPath.hashCode());
-		result = prime * result + ((targetObject == null) ? 0 : targetObject.hashCode());
-		result = prime * result + ((umlClonedNode == null) ? 0 : umlClonedNode.hashCode());
-		result = prime * result + ((umlClonedNodeSVP == null) ? 0 : umlClonedNodeSVP.hashCode());
-		result = prime * result + ((umlNodeToCloneSVP == null) ? 0 : umlNodeToCloneSVP.hashCode());
-		return result;
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public double getArchitecturalChanges() {
+		return numOfChanges;
 	}
 
 	@Override
@@ -310,29 +167,17 @@ public class UMLCloneNode extends UMLAddNodeActionImpl implements RefactoringAct
 		if (sourceModelPath == null) {
 			if (other.sourceModelPath != null)
 				return false;
-		} else if (!sourceModelPath.equals(other.sourceModelPath))
+		}
+
+		if (!targetElements.equals(other.targetElements))
 			return false;
-		if (targetObject == null) {
-			if (other.targetObject != null)
+
+		for (String k : createdElements.keySet()) {
+			if (!other.createdElements.get(k).iterator().next().equals(createdElements.get(k).iterator().next())) {
 				return false;
-		} else if (!targetObject.equals(other.targetObject))
-			return false;
-		if (umlClonedNode == null) {
-			if (other.umlClonedNode != null)
-				return false;
-		} else if (!umlClonedNode.equals(other.umlClonedNode))
-			return false;
-//		if (umlClonedNodeSVP == null) {
-//			if (other.umlClonedNodeSVP != null)
-//				return false;
-//		} else if (!umlClonedNodeSVP.equals(other.umlClonedNodeSVP))
-//			return false;
-//		if (umlNodeToCloneSVP == null) {
-//			if (other.umlNodeToCloneSVP != null)
-//				return false;
-//		} else if (!umlNodeToCloneSVP.equals(other.umlNodeToCloneSVP))
-//			return false;
+			}
+		}
+
 		return true;
 	}
-
 }
