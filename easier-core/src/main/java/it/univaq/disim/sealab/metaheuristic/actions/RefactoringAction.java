a0c58da57519be58e7f253a8715795012f1ea1ab
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-05-20 17:13:18 +0200

    update refactoring actions
    From now EMF models are no longer used to generate refatoring actions.

--- easier-core/src/main/java/it/univaq/disim/sealab/metaheuristic/actions/RefactoringAction.java
+++ easier-core/src/main/java/it/univaq/disim/sealab/metaheuristic/actions/RefactoringAction.java
@@ -1,13 +1,14 @@
 package it.univaq.disim.sealab.metaheuristic.actions;
 
+import it.univaq.disim.sealab.epsilon.eol.EasierUmlModel;
+import it.univaq.disim.sealab.metaheuristic.utils.EasierException;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 public interface RefactoringAction extends Cloneable {
 
-//	@Deprecated
-//	public RefactoringAction clone(RSolution solution);
 	public RefactoringAction clone();
 
 	public String getTargetType();
@@ -19,10 +20,14 @@ public interface RefactoringAction extends Cloneable {
 
 	public  String getName();
 
-	public void execute() throws RuntimeException;
+	public void execute(EasierUmlModel model) throws EasierException;
 
 	public double getArchitecturalChanges();
 
-	public double computeArchitecturalChanges(Collection<?> modelContents);
+	public double computeArchitecturalChanges(Collection<?> modelContents) throws EasierException;
+
+	public void setIndependent(Map<String, Set<String>> initialElements);
+
+	public boolean isIndependent();
 
 }
--- easier-uml/src/main/java/it/univaq/disim/sealab/metaheuristic/actions/uml/RefactoringActionFactory.java
+++ easier-uml/src/main/java/it/univaq/disim/sealab/metaheuristic/actions/uml/RefactoringActionFactory.java
@@ -1,6 +1,8 @@
 package it.univaq.disim.sealab.metaheuristic.actions.uml;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Set;
 
 import it.univaq.disim.sealab.metaheuristic.utils.Configurator;
 import org.uma.jmetal.util.pseudorandom.JMetalRandom;
@@ -11,18 +13,19 @@ import it.univaq.disim.sealab.metaheuristic.evolutionary.UMLRSolution;
 
 public class RefactoringActionFactory {
 
-    public static RefactoringAction getRandomAction(UMLRSolution sol) {
+    // TODO change param. Instead of using UMRSolution, use availableElements, and initialElements directly
+    public static RefactoringAction getRandomAction(Map<String, Set<String>> availableElements, Map<String, Set<String>> initialElements) {
 
         int extractedAction = JMetalRandom.getInstance().nextInt(0, 3);
         switch (extractedAction) {
             case 0:
-                return new UMLCloneNode(sol.getModelPath().toString(),sol.getAvailableElements());
+                return new UMLCloneNode(availableElements, initialElements);
             case 1:
-                return new UMLMvComponentToNN(sol.getModelPath().toString(),sol.getAvailableElements());
+                return new UMLMvComponentToNN(availableElements, initialElements);
             case 2:
-                return new UMLMvOperationToNCToNN(sol.getModelPath().toString(),sol.getAvailableElements());
+                return new UMLMvOperationToNCToNN(availableElements, initialElements);
             case 3:
-                return new UMLMvOperationToComp(sol.getModelPath().toString(),sol.getAvailableElements());
+                return new UMLMvOperationToComp(availableElements, initialElements);
             default:
                 return null;
         }
