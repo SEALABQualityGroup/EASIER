7c0ea0855a415054602e67d55fd9d6224a1153b0
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-05-20 17:09:07 +0200

    update evolutionary operators
    update xOver and mutation.
    From now the operators use map instead of emf models.

--- easier-core/src/main/java/it/univaq/disim/sealab/metaheuristic/evolutionary/operator/RMutation.java
+++ easier-core/src/main/java/it/univaq/disim/sealab/metaheuristic/evolutionary/operator/RMutation.java
@@ -1,89 +1,50 @@
 package it.univaq.disim.sealab.metaheuristic.evolutionary.operator;
 
+import it.univaq.disim.sealab.metaheuristic.evolutionary.RSolution;
+import it.univaq.disim.sealab.metaheuristic.utils.Configurator;
 import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.util.JMetalException;
-import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
-import it.univaq.disim.sealab.metaheuristic.actions.Refactoring;
-import it.univaq.disim.sealab.metaheuristic.evolutionary.RProblem;
-import it.univaq.disim.sealab.metaheuristic.evolutionary.RSolution;
+public abstract class RMutation<S extends RSolution<?>> implements MutationOperator<S> {
 
-public class RMutation<S extends RSolution<?>> implements MutationOperator<S> {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private static final double DEFAULT_PROBABILITY = 0.01;
-	private static final double DEFAULT_DISTRIBUTION_INDEX = 20.0;
-	private final double distributionIndex;
-	private final double mutationProbability;
-	private RepairRSolution solutionRepair;
+    private static final long serialVersionUID = 1L;
 
-	/** Constructor */
-	public RMutation() {
-		this(DEFAULT_PROBABILITY, DEFAULT_DISTRIBUTION_INDEX);
-	}
+    private static final double DEFAULT_PROBABILITY = 0.01;
+    private static final double DEFAULT_DISTRIBUTION_INDEX = 20.0;
+    private final double distributionIndex;
+    private final double mutationProbability;
+    private RepairRSolution solutionRepair;
 
-	/** Constructor */
-	public RMutation(RProblem<S> problem, double distributionIndex) {
-		this(1.0 / problem.getNumberOfVariables(), distributionIndex);
-	}
+    /**
+     * Constructor
+     */
+    public RMutation(double mutationProbability, double distributionIndex) {
+//		this(mutationProbability, distributionIndex, new RepairRSolutionAtBounds());
+        this.mutationProbability = mutationProbability;
+        this.distributionIndex = distributionIndex;
+    }
 
-	/** Constructor */
-	public RMutation(double mutationProbability, double distributionIndex) {
-		this(mutationProbability, distributionIndex, new RepairRSolutionAtBounds());
-	}
+    /* Getters */
+    public double getMutationProbability() {
+        return mutationProbability;
+    }
 
-	/** Constructor */
-	public RMutation(double mutationProbability, double distributionIndex, RepairRSolution solutionRepair) {
-		if (mutationProbability < 0) {
-			throw new JMetalException("Mutation probability is negative: " + mutationProbability);
-		} else if (distributionIndex < 0) {
-			throw new JMetalException("Distribution index is negative: " + distributionIndex);
-		}
-		this.mutationProbability = mutationProbability;
-		this.distributionIndex = distributionIndex;
+    /**
+     * Execute() method
+     */
+    @Override
+    public S execute(S solution) throws JMetalException {
+        if (null == solution) {
+            throw new JMetalException("Null parameter");
+        }
+        int allowed = Configurator.eINSTANCE.getAllowedFailures();
+        doMutation(mutationProbability, solution, allowed);
+        return solution;
+    }
 
-	}
-
-	/* Getters */
-	public double getMutationProbability() {
-		return mutationProbability;
-	}
-
-	/** Execute() method */
-	@Override
-	public S execute(S solution) throws JMetalException {
-		if (null == solution) {
-			throw new JMetalException("Null parameter");
-		}
-		int allowed = 1000;
-		doMutation(mutationProbability, solution, allowed);
-		return solution;
-	}
-
-	/** Perform the mutation operation */
-	private void doMutation(double probability, S solution, int allowed_failures) {
-
-		for (int i = 0; i < solution.getNumberOfVariables(); i++) {
-			if (JMetalRandom.getInstance().nextDouble() <= probability) {
-				boolean altered = false;
-				int num_failures = 0;
-				while (!altered) {
-					if (solution.alter(JMetalRandom.getInstance().nextInt(0,
-							((Refactoring) solution.getVariable(0)).getActions().size() - 1))) {
-						solution.setMutated();
-						break;
-					} else {
-						num_failures++;
-						if (num_failures >= allowed_failures)
-							break;
-					}
-				}
-			}
-		}
-	}
-
+    /**
+     * Perform the mutation operation
+     */
+    protected abstract void doMutation(double mutationProbability, S solution, int allowed_failures);
 
 }
